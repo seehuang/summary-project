9e8b1b664d30c6bf70e254bf1bd1e669c1980f1b
iiling <136970135@qq.com>
2018-02-08 16:06:05 +0800

    read

--- common config/config/config.js
+++ common config/config/config.js
@@ -3,7 +3,7 @@
 * @Date:   2018-02-05 11:04:26
 * @Last Modified by:   hc
 * @description 多页应用的静态资源加载的配置，主要是配置JS,CSS,Icon静态文件的自动加载，并在文件末尾添加版本号。
-* @Last Modified time: 2018-02-05 11:42:12
+* @Last Modified time: 2018-02-06 15:36:38
 */
 window.My = window.My || {};
 
@@ -27,6 +27,26 @@ My.config = {
         var d = new Date();
         var version = d.getFullYear()+''+('0'+(d.getMonth()+1)).substr(-2,2)+''+('0'+d.getDate()).substr(-2,2) +'_'+Math.random();
         return this.siteConfig.version;
+    },
+    /**
+     * @Author   hc
+     * @DateTime 2018-02-06
+     * @Describe 获取当前时间  格式 2018-01-01 11:02:59
+     * @return   {[type]}   [description]
+     */
+    getCurrentTime(){
+        var myDate = new Date();
+        var y = myDate.getFullYear();    //获取完整的年份(4位,1970-????)
+        var m = myDate.getMonth()+1;       //获取当前月份(0-11,0代表1月)
+        var d = myDate.getDate();        //获取当前日(1-31)
+        m=m>10?m:("0"+m);
+        d=d>10?d:("0"+d);
+        var hh= myDate.getHours();       //获取当前小时数(0-23)
+        var mm = myDate.getMinutes();     //获取当前分钟数(0-59)
+        var ss = myDate.getSeconds();     //获取当前秒数(0-59)
+        var sss = myDate.getMilliseconds();    //获取当前毫秒数(0-999)
+        var time =""+y+"-"+m+"-"+d+" "+hh+":"+mm+":"+ss;
+        return time;
     },
     getDomain:function(key){
         return this.domainList['global'][key];
